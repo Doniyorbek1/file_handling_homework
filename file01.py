f56570cd647dbf75a096e1a55f2579c39885eb25
Doniyorbek <ndoniyorbek1@gmail.com>
2022-11-09 11:28:56 +0500

    faylni funksiyaga ro'yhat ko'rinishida,qiymat qilib yuboryapmiz

--- file01.py
+++ file01.py
@@ -1,3 +1,5 @@
+
+
 def main(data:str):
     """
     The data is from the file. Return data as a list type.
@@ -6,5 +8,8 @@ def main(data:str):
     Returns:
         list: return answer
     """
-
+    data =f.split(',')
+    return data
 # Read data from file
+f= open('/home/asus/Python/file_handling_homework/txt_file/data01.txt','r').read()
+print(main(f))
