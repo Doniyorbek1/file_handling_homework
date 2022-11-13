8a127ecd39717ad94a3d5ac17a47aed6baf0ed2a
Doniyorbek <ndoniyorbek1@gmail.com>
2022-11-13 16:18:24 +0500

    fayldagi elementlardan tashkl topgan list

--- file06.py
+++ file06.py
@@ -6,5 +6,12 @@ def main(data:str):
     Returns:
         list: return answer
     """
+    data = cd.split()
+    list1 = []
+    for i in data:
+        list1 = list1 + [len(i)]
+    return list1
     
 # Read data from file
+cd = open('txt_file/data06.txt').read()
+print(main(cd))
