6eab8ab7c22854ddd99a913ccb9af726fee0403f
Doniyorbek <ndoniyorbek1@gmail.com>
2022-11-13 16:45:44 +0500

    file dagi uzun qator uzunligi:

--- file10.py
+++ file10.py
@@ -6,5 +6,13 @@ def main(data:str):
     Returns:
         int: return answer
     """
+    data = 0
+    for i in op:
+        if len(i)>data:
+            data = len(i)
+    
 
+    return data
 # Read data from file
+op = open('txt_file/data10.txt').read().split('\n')
+print(main(op))
