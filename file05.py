4dd05060f3fe11b57ffb92c2364e91a5b317af9f
Doniyorbek <ndoniyorbek1@gmail.com>
2022-11-13 17:25:04 +0500

    raqamlar va belgilar sonini chiqarish

--- file05.py
+++ file05.py
@@ -6,5 +6,17 @@ def main(data:str):
     Returns:
         list: return answer
     """
+    list1 = []
+    num = 0
+    char = 0
+    for i in opn[0]:
+        if i.isdigit():
+            num +=1
+        else:
+            char +=1
+    list1 = list1 + [num] + [char]
+    return list1
     
 # Read data from file
+opn = open('txt_file/data05.txt').read().split(',')
+print(main(opn))
