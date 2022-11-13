4f9df5c66a8f08239e7c073ce33164617015598f
Doniyorbek <ndoniyorbek1@gmail.com>
2022-11-13 16:16:48 +0500

    fayldagi raqamlar yig'indisi

--- file07.py
+++ file07.py
@@ -6,5 +6,17 @@ def main(data:str):
     Returns:
         int: return answer
     """
-    
+    data = f.split(',')
+    sum = 0
+    num = []
+
+    for i in data:
+        for j in i:
+            num = num +[j]
+    for i in num:
+        if i.isdigit():
+            sum = sum + int(i)
+    return sum
 # Read data from file
+f = open('txt_file/data07.txt').read()
+print(main(f))
