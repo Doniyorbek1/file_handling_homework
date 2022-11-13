2b799256c6597a462f5140de419dffd076c80bd5
Doniyorbek <ndoniyorbek1@gmail.com>
2022-11-13 16:40:06 +0500

    datadagi kichik raqam

--- file09.py
+++ file09.py
@@ -6,5 +6,16 @@ def main(data:str):
     Returns:
         int: return answer
     """
+    data = []
+    num = 0
+    for i in data1:
+        if i.isnumeric():
+            data = data + [i]    
+    for i in data:
+        if int(i)<num:
+            num = int(i)
+    return num
 
 # Read data from file
+data1 = open('txt_file/data09.txt').read().split()
+print(main(data1))
