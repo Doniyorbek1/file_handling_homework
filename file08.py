276e975d18525107f0875bebf14f0ba11fd1ee7c
Doniyorbek <ndoniyorbek1@gmail.com>
2022-11-13 16:27:55 +0500

    data dagi katta sonni topish:

--- file08.py
+++ file08.py
@@ -6,5 +6,16 @@ def main(data:str):
     Returns:
         int: return answer
     """
+    data = []
+    num = 0
+    for i in f:
+        if i.isdigit():
+            data = data + [i]
+    for i in data:
+        if int(i)>num:
+            num = int(i)
+    return num
 
 # Read data from file
+f = open('txt_file/data08.txt').read().split()
+print(main(f))
